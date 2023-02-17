ca4b35373639235f48640cc588672ef58c3b51c0
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-17 17:07:01 +0000

    Add 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include<stdio.h>
+/**
+ * file: 4-puts.c
+ * author Jean Jimenez
+ */
+#include <stdio.h>
+#define
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
+puts("Programming is like building a multilingual puzzle \n");
+return(0);
 }
