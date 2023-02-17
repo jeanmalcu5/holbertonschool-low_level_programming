594f88407fb479d6882d03e018604c4482d53a17
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-17 16:37:17 +0000

    Add 4-push.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle\n"); return (0);
+int main
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
