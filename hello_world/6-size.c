e58f1ddfc73c1b1cd5fba3194a598235aa530e03
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-18 20:31:11 +0000

    Add 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
-int main() {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	// sizeof evaluates the size of a variable
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float: $lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return 0;
 }
