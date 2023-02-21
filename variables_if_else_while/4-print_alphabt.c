8066143e7a7c710d260acab116558d191a95c449
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-21 14:50:36 +0000

    Add 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - 
+ * Return: 
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter = 'z'; letter++)
+	{
+		if(letter = 'e' && letter !='q')
+			putchar(letter)
+	}
+	return(0)
+}
