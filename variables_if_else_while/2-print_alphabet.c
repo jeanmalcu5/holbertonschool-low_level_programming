5161c0d668607aba0b619cb61850fbd9a8a67eec
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-21 13:29:04 +0000

    Add 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the alphabet in lowercase, followed by a new line.
+ * Return: Always 0
+ */
+int main(void)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	return (0);
+}
