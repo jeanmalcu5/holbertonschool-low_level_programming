98aab8d51301ee35fea56e00cc0b22efc5bf7705
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-21 14:19:40 +0000

    Add 3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char letter;
+
+	for(letter='A'; <='Z'; letter++)
+		putchar(letter);
+
+	for(letter='a'; <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n')
+
+	return (0)
+}
