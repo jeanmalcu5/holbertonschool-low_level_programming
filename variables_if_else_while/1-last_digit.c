ac05ea150b427ba4fc19da79f1b5873b8927dd3c
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-21 13:21:50 +0000

    Add 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Prints whether the last digit of a random number is greater than 5, less than 6, or 0.
  * Return: Always 0
@@ -15,7 +14,7 @@ int main(void)
 
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is int and is greater than 5\'n,"
+		printf("Last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
