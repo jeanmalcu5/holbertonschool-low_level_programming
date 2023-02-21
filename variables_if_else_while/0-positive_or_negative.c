9ddfac6ed92f0f4a18d74759be005c0152f46397
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-21 11:57:51 +0000

    Add 0-positive_or-negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - prints whether the random number is positive, negative, or zero.
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
