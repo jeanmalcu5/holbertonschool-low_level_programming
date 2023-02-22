b04390c7470fb0d78fd674e19f88d2b909f75eb4
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-22 00:07:35 +0000

    Add 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
-
+#include <stdiuo.h>
 /**
- * main - print the alphabet in lowercase, followed by a new line.
+ * main - Prints the alphabet in lowercase.
  * Return: Always 0
  */
 int main(void)
