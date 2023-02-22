3853e22e2170d7b7859ff38fad8b731e1bccd0ee
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-22 00:20:34 +0000

    Add 4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
- * main - Prints all letters of alphabet, except q and e, in lowercase. Followed by a new line.
+ * main - Prints the alphabet in lowercase except q and e.
  * Return: Always 0
  */
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter !='q')
 			putchar(letter);
 	}
 	putchar('\n');
+
 	return (0);
 }
