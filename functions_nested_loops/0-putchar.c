478940e7ce2f5fd143d5740c484155489900190d
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-23 13:33:29 +0000

    Add 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 /**
- * main - 
- * Return:
+ * main - Prints _putchar, followed by a new line.
+ * Return: Always 0
  */
 int main(void)
 {
 	putchar(_putchar);
+
 	putchar('\n');
 
 	return (0);
