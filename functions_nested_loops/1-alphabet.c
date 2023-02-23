359b9eacbdce6ea9a63c111157921299e9de8e0a
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-23 16:49:05 +0000

    Add 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,13 @@
+#include
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
 
+	if ( letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
