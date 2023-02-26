b6889f4822b9f4c7adf339db002871c87ec4efc8
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-26 02:31:01 +0000

    Add 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - The Fizz-Bizz test.
+ * Return: Counting..
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else if (a % 5 == 0)
+
+			if (a != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}
