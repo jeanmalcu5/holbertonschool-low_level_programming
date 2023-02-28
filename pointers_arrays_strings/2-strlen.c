bc4e5602da339369b5e5556dff113977b4b89920
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-28 14:13:58 +0000

    Add 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strlen - returns the length of a string.
  * @s: char
  * Return: Always 0
  */
-int_strlen(char *s)
+int _strlen(char *s)
 {
 	_putchar(strlen(*s));
 	return(0);
