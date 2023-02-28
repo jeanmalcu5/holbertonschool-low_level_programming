d12c9d5021f6eedd0d9882baf77e15b269d73d45
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-28 17:40:39 +0000

    Add 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - Takes a pointer to an int and updates the value to 98.
- * @n: int.
+ * reset_to_98 - Takes a pointer to an int and updates the value to 98.
+ * @n: int
  */
 void reset_to_98(int *n)
 {
---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdout.h>
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str:
+ */
+void _puts(char *str)
+{
+}
