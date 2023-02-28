c15786a2659d04d8fd7a7f9a1849690598ce3cdd
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-02-28 13:50:20 +0000

    Add 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Takes a pointer to an int as parameter and updates the value it points to to 98.
- * @n: Points to an int
+ * @n: int.
  */
 void reset_to_98(int *n);
 {
