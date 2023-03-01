0502a8128ead84d0252b42a62a83e8ab3b53fb33
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-03-01 02:03:29 +0000

    Add 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: giving string pointer
+ */
+void puts_half(char *str)
+{
+	int lenght, even, odd;
+
+	lenght = 0;
+
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+	if (lenght % 2 == 0)
+	{
+		for (even = lenght / 2; str[even] != '\0'; even++)
+		{
+			_putchar(str[even]);
+		}
+	}
+	else
+	{
+		for (odd = (lenght - 1) / 2; odd < lenght - 1; odd++)
+		{
+		_putchar(str[odd]);
+		}
+	}
+	_putchar('\n');
+}
