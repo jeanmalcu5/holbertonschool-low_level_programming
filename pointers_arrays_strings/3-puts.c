07388690241302a94b3748745372ac3ddaef1210
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-03-01 01:48:09 +0000

    Add 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,15 @@
 #include "main.h"
-#include <stdout.h>
 /**
  * _puts - prints a string, followed by a new line, to stdout.
- * @str:
+ * @str: char
  */
 void _puts(char *str)
 {
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+	{
+		_putchar(str[string]);
+	}
+	_putchar('\n');
 }
