1a709d9a2ae4c9034d625d53a34f1352498def87
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-03-07 13:01:20 +0000

    Add 0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s:
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	char s[] = "this is a string";
+
+	puts(s);
+
+	return (0);
+}
