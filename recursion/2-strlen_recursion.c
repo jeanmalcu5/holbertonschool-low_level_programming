cd5344d6654d23fd73a66d386d339317f560f82f
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-03-07 18:23:57 +0000

    Add 2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: char
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; ++i);
+
+	_putchar(*s);
+	return (*S);
+}
