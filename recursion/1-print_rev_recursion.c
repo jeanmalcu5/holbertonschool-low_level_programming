640628693aec845a00736b3924215771932f565f
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-03-07 18:01:16 +0000

    Add 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: char
+ * Return: reversed string
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0');
-	{
-		_putchar(*s);
+	if (*s == '\0)
 		return;
-	}
 
 	else
 	{
