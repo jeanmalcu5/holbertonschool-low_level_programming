598e18dcd0de491c11d18a1a1557e1ff9d218df6
Jean Jimenez <jeanmarcus619@gmail.comgit config --global user.name Jean>
2023-03-09 01:13:27 +0000

    Add 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: int
+ * Return: if n is lower than 0, the function should return -1 to indicate an error.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
