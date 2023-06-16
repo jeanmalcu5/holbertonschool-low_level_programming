988d55d30c61d9f72611452ad0f47b6dc36eca4e
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-16 17:35:02 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints size of various types on the computer it is compiled and run on.
+ * main - prints size of various types on the computer.
  * Return: Always 0.
  */
 int main(void)
