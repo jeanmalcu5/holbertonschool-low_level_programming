3836a0888e8196505002e9133a665fa1f4409b54
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-16 16:18:20 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints prints exactly "Programming is like building a multilingual puzzle", followed by a new line. 
- * Return: Always 0 
+ * main - prints input, followed by a new line.
+ * Return: Always 0.
  */
 int main(void)
 {
