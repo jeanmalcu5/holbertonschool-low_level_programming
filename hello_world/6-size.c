41510a000487dcf4769f71bf0f042dc5c545c9bf
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-16 17:11:06 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - prints size of various types on the computer it is compiled and run on.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	return (0);
+}
