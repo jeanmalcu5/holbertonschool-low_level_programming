8d78f58a26d8d83ba194dd87b658eb8f8bc2f411
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-20 14:30:04 +0000

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - 
+ * Return: 
+ */
+int main(void)
+{
+	char 'a', 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
