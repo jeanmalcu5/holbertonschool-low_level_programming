d51075a0e68628a3e25007b3b337155f2dc7e5b0
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-20 17:32:19 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char n;
+	char l;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (l = 'A'; l <= 'F'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
