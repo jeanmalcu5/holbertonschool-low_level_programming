5ad13d9c3c2b7f80d9279672b0e4fe2c3137be9e
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-21 13:09:27 +0000

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 	int n;
-	int c = ',';
 
 	for (n= '0'; n < 10; n++)
 	{
 		putchar(n);
 		if (n < 9)
 		{
-			putchar(c );
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
