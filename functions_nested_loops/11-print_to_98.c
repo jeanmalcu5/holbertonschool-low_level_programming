013cb8a496a7f996042ca377e939d9aa84627ca7
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-23 19:22:33 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,11 +13,11 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d\n", n);
+				printf("%i\n", n);
 			}
 			else
 			{
-				printf("%d, ", n);
+				printf("%i, ", n);
 			}
 			n++;
 		}
@@ -28,11 +28,11 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d\n", n);
+				printf("%i\n", n);
 			}
 			else
 			{
-				printf("%d, ", n);
+				printf("%i, ", n);
 			}
 			n--;
 		}
