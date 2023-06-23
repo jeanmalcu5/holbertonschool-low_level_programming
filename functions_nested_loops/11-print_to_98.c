6f4d118030a3f11b92e673722117b12646683627
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-23 19:18:23 +0000

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_to_98 -
+ * @n:
+ * Return:
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n == 98)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			else
+			{
+				printf("%d, ", n)
+			}
+			n--;
+		}
+	}
+}
