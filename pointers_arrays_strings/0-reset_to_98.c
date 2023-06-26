04b52bf874a2a8429a1c5c94c23ea53d0b83e2f7
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-26 13:45:49 +0000

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 -
+ * reset_to_98 - takes a pointer to an int and values it at 98.
+ * @v: variable.
+ * @p: pointer.
+ * Return: 98.
  */
 void reset_to_98(int *n)
 {
