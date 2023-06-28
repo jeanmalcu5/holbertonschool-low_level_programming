37d16281a78ef995d8bdff2b617deb87467d2899
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-28 14:37:48 +0000

    100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _atoi -
+ * @s:
+ */
+int _atoi(char *s)
+{
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strcpy -
+ * @dest:
+ * @src:
+ */
+char *strcpy(char *dest, char *src)
+{
+}
