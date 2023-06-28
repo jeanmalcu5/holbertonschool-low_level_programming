87300e527c0a2b18304616cb48a2dfcec44da70e
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-28 14:35:43 +0000

    8

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: string being printed in half.
+ */
+void puts_half(char *str)
+{
+
+}
---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array on integers.
+ * @a:
+ * @n:
+ */
+void print_array(int *a, int n)
+{
+}
