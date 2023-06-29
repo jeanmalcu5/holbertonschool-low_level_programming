f6141d820fc77a7f796cef3e4e2dadea374983d7
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-29 12:57:48 +0000

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,5 +5,21 @@
  */
 void puts_half(char *str)
 {
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if ((i % 2) != 0)
+		i = i + 1;
+
+	i = i / 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar(10);
 }
