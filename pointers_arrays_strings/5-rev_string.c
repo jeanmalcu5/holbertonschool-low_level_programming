e2081de31151cef52c76c2a48960df50d76b9d73
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-06-30 14:42:39 +0000

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
  * rev_string - reverses a string.
- * @s: 
+ * @s: string.
  */
 void rev_string(char *s)
 {
+	int i;
+	int j;
+	int len;
+
 	for (j = len - 1; j >= 0; i--)
 	{
 		_putchar(s[j]);
