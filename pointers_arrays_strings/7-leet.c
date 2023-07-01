5e2d92a8e08a07ba0a7684ab198b08f278420372
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-01 03:43:02 +0000

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,8 +1,26 @@
 #include "main.h"
 /**
- * *leet - encodes a string into 1337.
- * @*:
+ * leet - encodes a string into 1337.
+ * @s: string.
+ * Return: value of the string.
  */
-char *leet(char *)
+char *leet(char *s)
 {
+	int i, j;
+
+	char let[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+
+	char num[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == let[j])
+			{
+				s[i] = num[j];
+			}
+		}
+	}
+	return (s);
 }
