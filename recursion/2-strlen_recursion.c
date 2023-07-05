32281782821353e2a1334c311de4d2b85478f889
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-05 12:12:37 +0000

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,9 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (s[0] == 0)
+	if (s[0] == '\0')
 	{
-		s++;
-		_strlen_recursion();
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
