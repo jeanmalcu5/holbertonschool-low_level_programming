c7047ce35511b9f29d4b69ee521f1ac14b1ca836
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-07 15:53:30 +0000

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv[0] = 0;
+	return (0);
+}
