f5ee57a0943612845521de21789d3d35e9d32156
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-07 01:04:36 +0000

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,9 +1,32 @@
 #include "main.h"
 /**
+ * checker - checks recursively the input from is_prime_number
+ * @n: iterator.
+ * @base: base number.
+ * Return: if n = prime number, return 1. Otherwise 0.
+ */
+int checker(int n, int base)
+{
+	if (base % n == 0 || base < 2)
+	{
+		return (0);
+	}
+	else if (n == base - 1)
+	{
+		return (1);
+	}
+	else if (base > n)
+	{
+		return (checker(n + 1, base));
+	}
+	return (1);
+}
+/**
  * is_prime_number - determines if the input integer is a prime number.
- * @n:
- * Return:
+ * @n: number.
+ * Return: if n = prime number, return 1. otherwise 0.
  */
 int is_prime_number(int n)
 {
+	return (checker(2, n));
 }
