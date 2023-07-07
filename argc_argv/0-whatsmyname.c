246e4f3dc1582e084f1adcae21c1b444992d5f9f
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-07 13:59:44 +0000

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the programs name, followed by a new line.
+ * @argc: argument count.
+ * @argv: argument vectors.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
