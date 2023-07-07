e1b7cb2f21c2ecff981f7d3315d6a830c13cef0b
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-07 00:57:16 +0000

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,9 +1,28 @@
 #include "main.h"
 /**
+ * checker - checks the input number from n to the base.
+ * @n: random number.
+ * @base: base number.
+ * Return: natural square root of base.
+ */
+int checker(int n, int base)
+{
+	if (n * n == base)
+	{
+		return (n);
+	}
+	if (n * n > base)
+	{
+		return (-1);
+	}
+	return (checker(n + 1, base));
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number.
- * @n:
- * Return:
+ * @n: number.
+ * Return: natural square root of n.
  */
 int _sqrt_recursion(int n)
 {
+	return (checker(1, n));
 }
