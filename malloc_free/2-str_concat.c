b6433481a6683614c5ba0f0220be75d1790daddc
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-13 18:11:12 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,4 +7,48 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *p = malloc(sizeof(char) * (i + j + 1));
+	int i = 0, j = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	else if (s2 == NULL)
+	{
+		s2  = "";
+	}
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		p[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	p[i] = '\0';
+
+	return (p);
 }
