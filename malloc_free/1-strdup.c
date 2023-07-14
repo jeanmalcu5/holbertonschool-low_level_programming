b7424a92a90eeeb3e3d4740c38ecad539bd3e325
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-14 20:45:37 +0000

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,30 +8,24 @@
  */
 char *_strdup(char *str)
 {
-	char *p;
 	int i = 0, j = 0;
+	char *p = malloc(sizeof(char) * (i + 1));
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-
-	p = malloc(sizeof(char) * (i + 1));
-
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-
 	for (j = 0; str[j]; j++)
 	{
 		p[j] = str[j];
 	}
-
 	return (p);
 }
