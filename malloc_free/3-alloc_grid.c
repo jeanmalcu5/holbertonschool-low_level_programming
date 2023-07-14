ee7eafbedbd7e0f72552ff4676a4200ab76890d2
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-14 21:30:44 +0000

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,31 +16,25 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	p = malloc(sizeof(int *) * height);
+	p = (int **)malloc(sizeof(int *) * height);
 
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int) * width);
-
+		p[i] = (int *)malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
-			for (; i >= 0; i--)
-			{
-				free(p[i]);
-			}
-			free(p);
 			return (NULL);
 		}
 	}
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; j++)
+		while (j < width)
 		{
+			j++;
 			p[i][j] = 0;
 		}
 	}
