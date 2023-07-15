004a41df4700690d0108ee18a5dbfcd70bc8b513
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-15 00:37:58 +0000

    2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name if the file it was compiled on, followed by a new
+ * line.
+ * Return: Always 0.
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
