66190162e2677f28eee6e10d2e7ffbd8701de23d
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-15 00:00:43 +0000

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -7,21 +7,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i;
 	char *p;
+	int i, j = nmemb * size;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	p = malloc(nmemb * size);
+	p = malloc(j);
 
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < l; i++)
 	{
 		p[i] = 0;
 	}
