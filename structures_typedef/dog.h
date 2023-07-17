785992d8a3fcecf9c4637f2f32b7fd1509d9a82b
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-07-17 16:52:48 +0000

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,16 @@
+#ifndef POPPY
+#define POPPY
 #include <stdio.h>
-
+/**
+ * dog - structure.
+ * @name: of the dog.
+ * @age: of the dog.
+ * @owner: of the dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+#endif
