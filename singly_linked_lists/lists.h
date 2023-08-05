7c0003b5ff9a6807441050cfca173d52780f30af
jeanmalcu5 <jeanmarcus619@gmail.com>
2023-08-05 01:43:52 +0000

    0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct lists_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct lists_s
+{
+char *str;
+unsigned int len;
+struct lists_s *next
+} lists_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
